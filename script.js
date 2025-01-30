1ffe2c8b21bdbee6bb36e0dcb602afd1143e99a7
Sai madhav <121184092+saimadhav874@users.noreply.github.com>
2025-01-30 13:33:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,explicit&type=single";
+const url =  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
     jokeContainer.classList.remove("fade");
